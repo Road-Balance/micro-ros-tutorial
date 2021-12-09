d41aa630bebd77a4dbf120e79ef552e8d99b2b18
kimsooyoung <tge1375@naver.com>
2021-12-09 17:17:00 +0900

    log added

--- Setup/docker_log.sh
+++ Setup/docker_log.sh
@@ -26,9 +26,19 @@ source install/setup.bash
 ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/ttyACM0
 # Insert USB
 
-ros2 topic echo /micro_ros_arduino_node_publisher
+log
+```
+[1639036158.968197] info     | TermiosAgentLinux.cpp | init                     | running...             | fd: 3
+[1639036158.968892] info     | Root.cpp           | set_verbose_level        | logger setup           | verbose_level: 4
+[1639036159.005203] info     | Root.cpp           | create_client            | create                 | client_key: 0x1C10CBEE, session_id: 0x81
+[1639036159.005439] info     | SessionManager.hpp | establish_session        | session established    | client_key: 0x1C10CBEE, address: 0
+[1639036159.044356] info     | ProxyClient.cpp    | create_participant       | participant created    | client_key: 0x1C10CBEE, participant_id: 0x000(1)
+[1639036159.045065] info     | ProxyClient.cpp    | create_topic             | topic created          | client_key: 0x1C10CBEE, topic_id: 0x000(2), participant_id: 0x000(1)
+[1639036159.045672] info     | ProxyClient.cpp    | create_publisher         | publisher created      | client_key: 0x1C10CBEE, publisher_id: 0x000(3), participant_id: 0x000(1)
+[1639036159.048888] info     | ProxyClient.cpp    | create_datawriter        | datawriter created     | client_key: 0x1C10CBEE, datawriter_id: 0x000(5), publisher_id: 0x000(3)
+```
 
-$ ros2 topic echo /std_msgs_msg_Int32
+ros2 topic echo /std_msgs_msg_Int32
 data: 38
 ---
 data: 39
